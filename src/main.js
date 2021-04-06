bf54dbca77814d11787cb2317cf5c2c7dfc804da
theogilardo <gilardotheo@gmail.com>
2021-04-06 17:16:59 +0200

    install hotjar

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Hotjar from "vue-hotjar";
 
 Vue.config.productionTip = false;
 
 Vue.filter("trunc", function(value) {
   return Math.trunc(value);
+});
+
+Vue.use(Hotjar, {
+  id: "2335217", // Hotjar Site ID
 });
 
 new Vue({
